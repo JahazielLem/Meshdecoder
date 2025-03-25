48a8b85730229ed57e4a993dcce3a7b42683dde8
JahazielLem <jahaziel.lem@gmail.com>
2025-03-24 23:05:54 -0600

    feat: add basic decryption
    Add the base decryption logic

--- dissector/meshtastic.c
+++ dissector/meshtastic.c
@@ -137,3 +137,200 @@ void proto_reg_handoff_meshtastic(void)
 {
   dissector_add_uint("wtap_encap", WTAP_ENCAP_USER1, handle_meshtastic);
 }
+/* meshtastic.c
+ *
+ * SPDX-FileCopyrightText: © 2025 Kevin Leon <kevinleon.morales@gmail.com>
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ */
+
+ #include <epan/packet.h>
+ #include <wiretap/wtap.h>
+ #include <wsutil/wsgcrypt.h>
+ 
+ #define MESHTASTIC_ADDR_LEN 4
+ #define MESHTASTIC_PKTID_LEN 4
+ #define MESHTASTIC_CIPHER_BLOCK_LEN 16
+ 
+ // Default 1 byte key
+ static const uint8_t psk_key[] = {0xd4, 0xf1, 0xbb, 0x3a, 0x20, 0x29,
+  0x07, 0x59, 0xf0, 0xbc, 0xff, 0xab,
+  0xcf, 0x4e, 0x69, 0x01};
+
+ // Dissector handles
+ static dissector_handle_t handle_meshtastic;
+ 
+ // Protocol handles
+ static int proto_meshtastic;
+ 
+ // Header field handles
+ static int hf_meshtastic_dst;
+ static int hf_meshtastic_src;
+ static int hf_meshtastic_pktid;
+ static int hf_meshtastic_flags;
+ static int hf_meshtastic_flags_hop_limit;
+ static int hf_meshtastic_flags_want_ack;
+ static int hf_meshtastic_flags_via_mqtt;
+ static int hf_meshtastic_flags_hop_start;
+ static int hf_meshtastic_channelhash;
+ static int hf_meshtastic_nexthop;
+ static int hf_meshtastic_relaynode;
+ static int hf_meshtastic_payload;
+ static int hf_meshtastic_decrypted_block;
+ 
+ // Subtree pointers
+ static int ett_header;
+ static int ett_flags;
+ 
+ static int dissect_meshtastic(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
+ {
+   int32_t current_offset = 0;
+ 
+   // Set columns
+   col_set_str(pinfo->cinfo, COL_PROTOCOL, "Meshtastic");
+   col_clear(pinfo->cinfo, COL_INFO);
+   col_clear(pinfo->cinfo, COL_DEF_DST);
+   col_clear(pinfo->cinfo, COL_DEF_SRC);
+ 
+   // Create subtrees
+   proto_item *ti = proto_tree_add_item(tree, proto_meshtastic, tvb, 0, -1, ENC_NA);
+   proto_tree *ti_radio = proto_item_add_subtree(ti, ett_header);
+ 
+   // Destination address
+   guint32 dst_addr = GUINT32_SWAP_LE_BE(tvb_get_ntohl(tvb, current_offset));
+   proto_tree_add_uint(ti_radio, hf_meshtastic_dst, tvb, current_offset, MESHTASTIC_ADDR_LEN, dst_addr);
+   col_add_fstr(pinfo->cinfo, COL_DEF_DST, "%08x", dst_addr);
+   current_offset += MESHTASTIC_ADDR_LEN;
+ 
+   // Source address
+   guint32 src_addr = GUINT32_SWAP_LE_BE(tvb_get_ntohl(tvb, current_offset));
+   uint32_t senderID = tvb_get_letohl(tvb, current_offset);
+   proto_tree_add_uint(ti_radio, hf_meshtastic_src, tvb, current_offset, MESHTASTIC_ADDR_LEN, src_addr);
+   col_add_fstr(pinfo->cinfo, COL_DEF_SRC, "%08x", src_addr);
+   current_offset += MESHTASTIC_ADDR_LEN;
+ 
+   // Packet ID
+   guint32 packetid = GUINT32_SWAP_LE_BE(tvb_get_ntohl(tvb, current_offset));
+   uint32_t packetID = tvb_get_letohl(tvb, current_offset);
+   proto_tree_add_uint(ti_radio, hf_meshtastic_pktid, tvb, current_offset, MESHTASTIC_PKTID_LEN, packetid);
+   current_offset += MESHTASTIC_PKTID_LEN;
+ 
+   // Flags
+   uint8_t flags_value = tvb_get_uint8(tvb, current_offset);
+   proto_item *ti_flags = proto_tree_add_uint_format(ti_radio, hf_meshtastic_flags, tvb, current_offset, 1, flags_value, "Flags: 0x%02x", flags_value);
+   proto_tree *field_tree = proto_item_add_subtree(ti_flags, ett_flags);
+   proto_tree_add_uint(field_tree, hf_meshtastic_flags_hop_limit, tvb, current_offset, 1, flags_value & 0b111);
+   proto_tree_add_boolean(field_tree, hf_meshtastic_flags_want_ack, tvb, current_offset, 1, (flags_value >> 3) & 0b1);
+   proto_tree_add_boolean(field_tree, hf_meshtastic_flags_via_mqtt, tvb, current_offset, 1, (flags_value >> 4) & 0b1);
+   proto_tree_add_uint(field_tree, hf_meshtastic_flags_hop_start, tvb, current_offset, 1, (flags_value >> 5) & 0b111);
+   current_offset += 1;
+ 
+   // Channel hash
+   uint8_t channel_hash = tvb_get_uint8(tvb, current_offset);
+   proto_tree_add_uint(ti_radio, hf_meshtastic_channelhash, tvb, current_offset, 1, channel_hash);
+   current_offset += 1;
+ 
+   // Next hop
+   proto_tree_add_item(ti_radio, hf_meshtastic_nexthop, tvb, current_offset, 1, ENC_NA);
+   current_offset += 1;
+ 
+   // Relay node
+   proto_tree_add_item(ti_radio, hf_meshtastic_relaynode, tvb, current_offset, 1, ENC_NA);
+   current_offset += 1;
+ 
+   // Payload
+   uint16_t payload_len = tvb_captured_length_remaining(tvb, current_offset);
+   proto_tree_add_item(ti_radio, hf_meshtastic_payload, tvb, current_offset, payload_len, ENC_NA);
+
+   /**
+    * Cipher block
+    * We need a nonce:
+    * packetid + 0x00\0x00\0x00\0x00\ + sender + 0x00\0x00\0x00\0x00
+    * Then use the default key to decrypt the payload. TODO: Add key input
+    */
+   uint8_t iv[MESHTASTIC_CIPHER_BLOCK_LEN]; // Nonce
+   memset(iv, 0, MESHTASTIC_CIPHER_BLOCK_LEN);
+   memcpy(iv, &packetID, sizeof(packetID));
+   memcpy(iv + MESHTASTIC_ADDR_LEN + MESHTASTIC_ADDR_LEN, &senderID, sizeof(senderID));
+
+   uint8_t* chiper_payload_block = tvb_memdup(pinfo->pool, tvb, current_offset, payload_len);
+
+   gcry_cipher_hd_t cipher_hd;
+   if (gcry_cipher_open(&cipher_hd, GCRY_CIPHER_AES128, GCRY_CIPHER_MODE_CTR, 0)){
+    col_set_str(pinfo->cinfo, COL_INFO, "[Failed cipher key]");
+    return tvb_captured_length(tvb);
+   }
+
+   if (gcry_cipher_setkey(cipher_hd, psk_key, MESHTASTIC_CIPHER_BLOCK_LEN)){
+    col_set_str(pinfo->cinfo, COL_INFO, "[Failed cipher key]");
+    gcry_cipher_close(cipher_hd);
+    return tvb_captured_length(tvb);
+   }
+
+   if (gcry_cipher_setctr(cipher_hd, iv, MESHTASTIC_CIPHER_BLOCK_LEN)){
+    col_set_str(pinfo->cinfo, COL_INFO, "[Failed cipher IV]");
+    gcry_cipher_close(cipher_hd);
+    return tvb_captured_length(tvb);
+   }
+
+   if (gcry_cipher_decrypt(cipher_hd, chiper_payload_block, payload_len, NULL, 0)) {
+    col_set_str(pinfo->cinfo, COL_INFO, "[Failed cipher decrypt]");
+    gcry_cipher_close(cipher_hd);
+    return tvb_captured_length(tvb);
+   }
+
+   gcry_cipher_close(cipher_hd);
+   
+   tvbuff_t* tvb_decrypted = tvb_new_child_real_data(tvb, chiper_payload_block, payload_len, payload_len);
+   tvb_decrypted = tvb_new_subset_length_caplen(tvb_decrypted, 0, payload_len, payload_len);
+
+   proto_tree_add_item(ti_radio, hf_meshtastic_decrypted_block, tvb_decrypted, 0, payload_len, ENC_NA);
+   add_new_data_source(pinfo, tvb_decrypted, "Decrypted Meshtastic Packet");
+   
+   return 0;
+ }
+ 
+ void proto_register_meshtastic(void)
+ {
+   // Setup a list of header fields
+   static hf_register_info hf[] = {
+       {&hf_meshtastic_dst, {"Destination", "meshtastic.dst", FT_UINT32, BASE_HEX, NULL, 0x0, "Destination Address", HFILL}},
+       {&hf_meshtastic_src, {"Source", "meshtastic.src", FT_UINT32, BASE_HEX, NULL, 0x0, "Source Address", HFILL}},
+       {&hf_meshtastic_pktid, {"Packet ID", "meshtastic.pktid", FT_UINT32, BASE_HEX, NULL, 0x0, NULL, HFILL}},
+       {&hf_meshtastic_flags, {"Flags", "meshtastic.flags", FT_UINT8, BASE_HEX, NULL, 0x0, NULL, HFILL}},
+       {&hf_meshtastic_flags_hop_limit, {"Hop Limit", "meshtastic.hop_limit", FT_UINT8, BASE_DEC, NULL, 0x07, NULL, HFILL}},
+       {&hf_meshtastic_flags_want_ack, {"Want Ack", "meshtastic.want_ack", FT_BOOLEAN, 8, NULL, 0x08, NULL, HFILL}},
+       {&hf_meshtastic_flags_via_mqtt, {"Via MQTT", "meshtastic.via_mqtt", FT_BOOLEAN, 8, NULL, 0x10, NULL, HFILL}},
+       {&hf_meshtastic_flags_hop_start, {"Hop Start", "meshtastic.hop_start", FT_UINT8, BASE_DEC, NULL, 0xE0, NULL, HFILL}},
+       {&hf_meshtastic_channelhash, {"Channel Hash", "meshtastic.channelhash", FT_UINT8, BASE_DEC_HEX, NULL, 0x0, NULL, HFILL}},
+       {&hf_meshtastic_nexthop, {"Next Hop", "meshtastic.nexthop", FT_UINT8, BASE_DEC, NULL, 0x0, NULL, HFILL}},
+       {&hf_meshtastic_relaynode, {"Relay Node", "meshtastic.relaynode", FT_UINT8, BASE_DEC, NULL, 0x0, NULL, HFILL}},
+       {&hf_meshtastic_payload, {"Payload", "meshtastic.payload", FT_BYTES, BASE_NONE, NULL, 0x0, NULL, HFILL}},
+       {&hf_meshtastic_decrypted_block, {"Decrypted", "meshtastic.decryp_block", FT_BYTES, BASE_NONE, NULL, 0x0, NULL, HFILL}},
+       
+   };
+ 
+   // Protocol subtrees array
+   static int *ett[] = {
+       &ett_header,
+       &ett_flags,
+   };
+ 
+   // Register protocol
+   proto_meshtastic = proto_register_protocol("Meshtastic", "Meshtastic", "meshtastic");
+ 
+   // Register dissectors
+   handle_meshtastic = register_dissector("meshtastic", dissect_meshtastic, proto_meshtastic);
+ 
+   // Register header fields
+   proto_register_field_array(proto_meshtastic, hf, array_length(hf));
+ 
+   // Register subtrees
+   proto_register_subtree_array(ett, array_length(ett));
+ }
+ 
+ void proto_reg_handoff_meshtastic(void)
+ {
+   dissector_add_uint("wtap_encap", WTAP_ENCAP_USER1, handle_meshtastic);
+ }
+ 
